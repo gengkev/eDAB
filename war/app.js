4668bd60b5473b76b73082f65f90d499522137a4
Kevin G. <gengkev@gmail.com>
2013-01-17 10:06:58 -0500

    Update war/app.js

--- war/app.js
+++ war/app.js
@@ -6,23 +6,27 @@ angular.module('project', [])
 			when('/settings', {controller: SettingsCtrl, templateUrl: 'html/settings.html'}).
 			otherwise({redirectTo: '/agenda'});
 	})
-	.factory('client', function($window) {
-		return {
+	.service('client', function($window) {
+		this.auth = {
 			login: function() {
 				$window.location.replace("login.jsp");
+			},
+			logged_in: false,
+			check: function() {
+				
 			}
 		};
 	})
 	.run(function(client) {
-		client.logged_in = false;
+		client.auth.check();
 	});
 
 function AppCtrl($scope, $location, client) {
 	$scope.$watch(function() { return client.logged_in; }, function(logged_in) {
-		$scope.logged_in = client.logged_in;
+		$scope.logged_in = client.auth.logged_in;
 	});
 	$scope.login = function() {
-		client.login();
+		client.auth.login();
 	};
 }
 
